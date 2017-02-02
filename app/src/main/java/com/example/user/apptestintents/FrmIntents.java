1ba61dee086e30b9fa73d628e265c285f8607e80
Octavian Dospinescu <doctav@uaic.ro>
2017-02-02 17:07:35 +0200

    first version

---
+++ app/src/main/java/com/example/user/apptestintents/FrmIntents.java
@@ -0,0 +1,13 @@
+package com.example.user.apptestintents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrmIntents extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_intents);
+    }
+}
